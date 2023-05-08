ca2d3495207be16276206a9767fe5f1d87de9581
gujh14 <rnwjdgus96@gmail.com>
2023-05-08 19:31:59 +0900

    added example python run command

--- script_rw.sh
+++ script_rw.sh
@@ -1,13 +1,8 @@
-for neg_dataset in "random" "TWOSIDES"
-do
-    for embeddingf in "node2vec" "edge2vec" "res2vec_hetero" "res2vec_homo" "DREAMwalk"
-    do
-        for train_mode in "contra" "nocontra"
-        do
-            for seed in {0..9}
-            do
-            python train_rw.py --database "DC_combined" --comb_type "prod_fc" --embeddingf $embeddingf --neg_dataset $neg_dataset --batch_size 128 --neg_ratio 1 --ce_lr 1e-3 --contra_lr 1e-1 --train_mode $train_mode --seed $seed
-            done
-        done
-    done
-done
+# Example command
+## embeddingf: ["node2vec", "edge2vec", "res2vec_hetero", "res2vec_homo", "DREAMwalk"]
+## neg_dataset: ["TWOSIDES", "random"]
+## neg_ratio: {1..3}
+## seed: {0..9}
+## train_mode: ["contra", "nocontra"]
+
+python train_rw.py --database "DC_combined" --comb_type "prod_fc" --embeddingf "DREAMwalk" --neg_dataset "TWOSIDES" --train_mode "contra" --neg_ratio 1 --batch_size 128 --ce_lr 1e-3 --contra_lr 1e-1 --seed 0
